1833b2e4991287238ab3186cd8712bc66cbd397b
Kyubre <163297244+Kyubre@users.noreply.github.com>
2024-11-06 11:06:47 +0100

    Map1 und Player aktualisiert
    OOP für Movement angewendet

--- Map1.java
+++ Map1.java
@@ -22,10 +22,6 @@ public class Map1 extends Application {
   private ImageView panzer = new ImageView();
   private Image panzerImage = new Image(getClass().getResourceAsStream("images/96.gif"));
   private Player p1 = new Player(panzer);
-  private boolean wGedrueckt = false;
-  private boolean aGedrueckt = false;
-  private boolean sGedrueckt = false;
-  private boolean dGedrueckt = false;
   private ImageView turret = new ImageView();
   private Image turretImage = new Image(getClass().getResourceAsStream("images/96.png"));
   private ImageView shot;
@@ -38,44 +34,14 @@ public class Map1 extends Application {
     primaryStage.setMaximized(true);
     primaryStage.resizableProperty();
     
-    scene.setOnKeyPressed((KeyEvent event) -> {
-      switch (event.getCode()) {
-        case W: 
-          wGedrueckt = true;
-          break;
-        case A: 
-          aGedrueckt = true;
-          break;
-        case S: 
-          sGedrueckt = true;
-          break;
-        case D: 
-          dGedrueckt = true;
-          break;
-        default: 
-          break;  
-      } // end of switch
-      updateMovement();
+    scene.setOnKeyPressed((KeyEvent event) -> {;
+      p1.tasteGedrueckt(event);
+      p1.movement(panzer, turret);
     });
     
-    scene.setOnKeyReleased((KeyEvent event) -> {
-      switch (event.getCode()) {
-        case W: 
-          wGedrueckt = false;
-          break;
-        case A: 
-          aGedrueckt = false;
-          break;
-        case S: 
-          sGedrueckt = false;
-          break;
-        case D: 
-          dGedrueckt = false;
-          break;
-        default: 
-          break;  
-      }
-      updateMovement();
+    scene.setOnKeyReleased((KeyEvent event) -> {;
+      p1.tasteLosgelassen(event);
+      p1.movement(panzer, turret);
     });
     
     scene.addEventFilter(MouseEvent.MOUSE_MOVED, event -> {;
@@ -121,12 +87,14 @@ public class Map1 extends Application {
     wall2.setHeight(120);
     wall2.setSmooth(false);
     root.getChildren().add(wall2);
+    
     panzer.setX(1448);
     panzer.setY(120);
     panzer.setFitWidth(120);
     panzer.setFitHeight(100);
     panzer.setImage(panzerImage);
     root.getChildren().add(panzer);
+    
     primaryStage.setResizable(false);
     
     turret.setX(1448);
@@ -143,58 +111,6 @@ public class Map1 extends Application {
   } // end of public Map1
   
   // Anfang Methoden
-  public void updateMovement() {
-    
-    if (wGedrueckt && aGedrueckt) {
-      panzer.setY(panzer.getY()-10);
-      panzer.setX(panzer.getX()-10);
-      panzer.setRotate(225);         
-      turret.setY(turret.getY()-10);
-      turret.setX(turret.getX()-10);
-    } 
-    else if (wGedrueckt && dGedrueckt) {
-      panzer.setY(panzer.getY()-10);
-      panzer.setX(panzer.getX()+10);
-      panzer.setRotate(315);
-      turret.setY(turret.getY()-10);
-      turret.setX(turret.getX()+10);
-    } 
-    else if (sGedrueckt && aGedrueckt) {
-      panzer.setY(panzer.getY()+10);
-      panzer.setX(panzer.getX()-10);
-      panzer.setRotate(135);
-      turret.setY(turret.getY()+10);
-      turret.setX(turret.getX()-10);
-    } 
-    else if (sGedrueckt && dGedrueckt) {
-      panzer.setY(panzer.getY()+10);
-      panzer.setX(panzer.getX()+10);
-      panzer.setRotate(45);
-      turret.setY(turret.getY()+10);
-      turret.setX(turret.getX()+10);
-    } 
-    else if (wGedrueckt) {
-      panzer.setY(panzer.getY()-10);
-      panzer.setRotate(270);
-      turret.setY(turret.getY()-10);
-    } 
-    else if (aGedrueckt) {
-      panzer.setX(panzer.getX()-10);
-      panzer.setRotate(180);
-      turret.setX(turret.getX()-10);
-    } 
-    else if (sGedrueckt) {
-      panzer.setY(panzer.getY()+10);
-      panzer.setRotate(90);
-      turret.setY(turret.getY()+10);
-    } 
-    else if (dGedrueckt) {
-      panzer.setX(panzer.getX()+10);
-      panzer.setRotate(0);
-      turret.setX(turret.getX()+10);
-    }
-  }
-
   
   public static void main(String[] args) {
     launch(args);
--- Player.java
+++ Player.java
@@ -1,12 +1,17 @@
 import javafx.scene.input.MouseEvent;
 import javafx.scene.image.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 
 public class Player{
   private double xPos;
   private double yPos;
   private double centerPosX;
   private double centerPosY;
+  private boolean wGedrueckt = false;
+  private boolean aGedrueckt = false;
+  private boolean sGedrueckt = false;
+  private boolean dGedrueckt = false;
   
   public Player(ImageView panzer){
     this.xPos = panzer.getX();
@@ -32,6 +37,96 @@ public class Player{
   
   public void shoot(Player panzer){
     
+  }
+  
+  public void tasteGedrueckt(KeyEvent evt){
+    switch (evt.getCode()) {
+        case W: 
+          wGedrueckt = true;
+          break;
+        case A: 
+          aGedrueckt = true;
+          break;
+        case S: 
+          sGedrueckt = true;
+          break;
+        case D: 
+          dGedrueckt = true;
+          break;
+        default: 
+          break;  
+      }
+  }
+  
+  public void tasteLosgelassen(KeyEvent evt){
+    switch (evt.getCode()) {
+        case W: 
+          wGedrueckt = false;
+          break;
+        case A: 
+          aGedrueckt = false;
+          break;
+        case S: 
+          sGedrueckt = false;
+          break;
+        case D: 
+          dGedrueckt = false;
+          break;
+        default: 
+          break;  
+      }
+  }
+
+  
+  public void movement(ImageView panzer, ImageView turret){    
+    if (wGedrueckt && aGedrueckt) {
+      panzer.setY(panzer.getY()-10);
+      panzer.setX(panzer.getX()-10);
+      panzer.setRotate(225);         
+      turret.setY(turret.getY()-10);
+      turret.setX(turret.getX()-10);
+    } 
+    else if (wGedrueckt && dGedrueckt) {
+      panzer.setY(panzer.getY()-10);
+      panzer.setX(panzer.getX()+10);
+      panzer.setRotate(315);
+      turret.setY(turret.getY()-10);
+      turret.setX(turret.getX()+10);
+    } 
+    else if (sGedrueckt && aGedrueckt) {
+      panzer.setY(panzer.getY()+10);
+      panzer.setX(panzer.getX()-10);
+      panzer.setRotate(135);
+      turret.setY(turret.getY()+10);
+      turret.setX(turret.getX()-10);
+    } 
+    else if (sGedrueckt && dGedrueckt) {
+      panzer.setY(panzer.getY()+10);
+      panzer.setX(panzer.getX()+10);
+      panzer.setRotate(45);
+      turret.setY(turret.getY()+10);
+      turret.setX(turret.getX()+10);
+    } 
+    else if (wGedrueckt) {
+      panzer.setY(panzer.getY()-10);
+      panzer.setRotate(270);
+      turret.setY(turret.getY()-10);
+    } 
+    else if (aGedrueckt) {
+      panzer.setX(panzer.getX()-10);
+      panzer.setRotate(180);
+      turret.setX(turret.getX()-10);
+    } 
+    else if (sGedrueckt) {
+      panzer.setY(panzer.getY()+10);
+      panzer.setRotate(90);
+      turret.setY(turret.getY()+10);
+    } 
+    else if (dGedrueckt) {
+      panzer.setX(panzer.getX()+10);
+      panzer.setRotate(0);
+      turret.setX(turret.getX()+10);
+    }
   }
 
   
