37a8e74d8a197479ca8745ed2b8ae7f384c2b1cc
kyubre <kyubre@proton.me>
2024-11-14 11:13:21 +0100

    Push von Niklas Laptop

--- Gegner.java
+++ Gegner.java
@@ -1,5 +1,6 @@
 import javafx.scene.shape.Rectangle;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Line;
 
 public class Gegner {
   public Gegner(){
@@ -12,86 +13,7 @@ public class Gegner {
   }
   
   public boolean siehtSpieler(ImageView spieler, ImageView gegner, Rectangle wand1, Rectangle wand2){
-    //Gegner unten links von Spieler
-    if (gegner.getX() < spieler.getX() && gegner.getY() < spieler.getY()) {
-      for (double i = gegner.getX(); i < spieler.getX(); i++) {
-        for (double j = gegner.getY(); j < spieler.getY(); j++) {
-          if (!wand1.intersects(i,j,1,1) && !wand2.intersects(i,j,1,1)) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-    //Gegner oben links vom Spieler
-    else if (gegner.getX() < spieler.getX() && gegner.getY() > spieler.getY()) {
-      for (double i = gegner.getX(); i < spieler.getX(); i++) {
-        for (double j = gegner.getY(); j > spieler.getY(); j++) {
-          if (!wand1.intersects(i,j,1,1) && !wand2.intersects(i,j,1,1)) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-    //Gegner unten rechts von Spieler
-    else if (gegner.getX() > spieler.getX() && gegner.getY() > spieler.getY()) {
-      for (double i = gegner.getX(); i > spieler.getX(); i++) {
-        for (double j = gegner.getY(); j > spieler.getY(); j++) {
-          if (!wand1.intersects(i,j,1,1) && !wand2.intersects(i,j,1,1)) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-    //Gegner oben rechts von Spieler
-    else if (gegner.getX() > spieler.getX() && gegner.getY() < spieler.getY()) {
-      for (double i = gegner.getX(); i > spieler.getX(); i++) {
-        for (double j = gegner.getY(); j < spieler.getY(); j++) {
-          if (!wand1.intersects(i,j,1,1) && !wand2.intersects(i,j,1,1)) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-    //Gegner unten mittig von Spieler
-    else if (gegner.getY() > spieler.getY()) {
-      for (double j = gegner.getY(); j > spieler.getY(); j++) {
-        if (!wand1.intersects(spieler.getX(),j,1,1) && !wand2.intersects(spieler.getX(),j,1,1)) {
-          return true;
-        }
-      }
-      return false;
-    }
-    //Gegner oben mittig von Spieler
-    else if (gegner.getY() < spieler.getY()) {
-      for (double j = gegner.getY(); j < spieler.getY(); j++) {
-        if (!wand1.intersects(spieler.getX(),j,1,1) && !wand2.intersects(spieler.getX(),j,1,1)) {
-          return true;
-        }
-      }
-      return false;
-    }
-    //Gegner mittig links vom Spieler
-    else if (gegner.getX() < spieler.getX()) {
-      for (double i = gegner.getX(); i < spieler.getX(); i++) {
-        if (!wand1.intersects(i,spieler.getY(),1,1) && !wand2.intersects(i,spieler.getY(),1,1)) {
-          return true;
-        }
-      }
-      return false;
-    }
-    //Gegner mittig rechts vom Spieler
-    else if (gegner.getX() > spieler.getX()) {
-      for (double i = gegner.getX(); i > spieler.getX(); i++) {
-        if (!wand1.intersects(i,spieler.getY(),1,1) && !wand2.intersects(i,spieler.getY(),1,1)) {
-          return true;
-        }
-      }
-      return false;
-    }
+    Line lineOfSight = new Line(gegner.getX(), gegner.getY(), spieler.getX(), spieler.getY());
     return false;
   }
 
